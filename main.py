ae9ac5ba0b5581e06747d8f4c304c8ffca94a661
Naveen Pasupuleti <naveenroyal12345@gmail.com>
2024-08-02 00:38:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 # Load the IMDB dataset word index
 word_index = imdb.get_word_index()
